94e9ae1595e0f4b84e075338333645cc1734f410
KarolannMauger <karolannmauger@gmail.com>
2024-03-20 12:03:54 -0400

    Chnagement nom des images et modification / ajout dans le code

--- CodeTelecommande/Codes/CastVideos1.js
+++ CodeTelecommande/Codes/CastVideos1.js
@@ -16,10 +16,11 @@ const videoList = [
     'https://transfertco.ca/video/usa23_7_02.mp4'
 ];
 
+//Connection
 document.getElementById('cast_connect').addEventListener('click', () => {
     initializeApiOnly();
 });
-
+//Start 
 document.getElementById('start_button').addEventListener('click', () => {
     if (currentSession) {
         if(localStorage.getItem('currentVideoIndexLS')) {
@@ -34,6 +35,7 @@ document.getElementById('start_button').addEventListener('click', () => {
     }
 });
 
+//NEXT / PREVIOUS
 document.getElementById('next_button').addEventListener('click', () => {
     if (currentSession) {
         currentVideoIndex = (currentVideoIndex + 1) % videoList.length;
@@ -54,6 +56,7 @@ document.getElementById('previousBtn').addEventListener('click', () => {
     }
 });
 
+//PLAY / PAUSE
 document.getElementById('play_button').addEventListener('click', () => {
     if (currentMediaSession) {
         if (isPlaying) {
@@ -64,6 +67,40 @@ document.getElementById('play_button').addEventListener('click', () => {
         isPlaying = !isPlaying;
     }
 });
+//NEW PLAY / PAUSE vient de moi
+document.getElementById('playBtn').addEventListener('click', () => {
+    if (currentMediaSession) {
+        if (!isPlaying) {
+            currentMediaSession.play(null, onMediaCommandSuccess, onError);
+        isPlaying = !isPlaying;
+        }
+    }
+});
+
+document.getElementById('pauseBtn').addEventListener('click', () => {
+    if (currentMediaSession) {
+        if (isPlaying) {
+            currentMediaSession.pause(null, onMediaCommandSuccess, onError);
+        }
+        isPlaying = !isPlaying;
+    }
+});
+
+//Volume
+
+//New mute function. Elle est meilleur que la précédente, car quand on click sur le button, il a pas de freeze et nous voyons avec l'état de volume sur la vidéo elle est mute.
+//Je suis allé la chercher dans mon propre code de télécommande.
+let isMuted = false;
+document.getElementById('muteToggle').addEventListener('click', () => {
+    if (currentSession) {
+        isMuted = !isMuted;
+        currentSession.setReceiverMuted(isMuted, onMediaCommandSuccess, onError);
+    }
+});
+
+//Volume +
+
+//Volume -
 
 
 function sessionListener(newSession) {
